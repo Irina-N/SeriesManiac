4c081bcc46d9c49683ff7a3bc403dc071ff4728f
Irina-N <i.narozniaja@gmail.com>
2021-08-12 01:50:59 +0300

    Try №2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 require('./bootstrap');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../css/app.css';
+import Layout from './Layout';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Layout />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,27 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{'Laravel 8 with React 17 Boilerplate'}}</title>
+
+    <!-- Styles -->
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="root">
+   
+</div>
+<!-- Scripts -->
+<script src="{{ mix('/js/app.js') }}"></script>
+</body>
+</html>
+
+<!-- 
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -129,4 +151,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> -->
