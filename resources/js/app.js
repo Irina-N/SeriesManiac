9146bc85618f6748e20c6c7ef737bb4b95cd25b5
Irina-N <i.narozniaja@gmail.com>
2021-08-12 16:18:13 +0300

    Laravel + React

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 require('./bootstrap');
+
+/* require('./components/Example'); */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../css/app.css';
+import Layout from './components/Layout';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Layout />
+  </React.StrictMode>,
+  document.getElementById('app')
+);
---
+++ resources/js/components/Layout.js
@@ -0,0 +1,11 @@
+import '../../css/Layout.css';
+
+function Layout() {
+  return (
+    <div className="Layout">
+      Hello
+    </div>
+  );
+}
+
+export default Layout;
