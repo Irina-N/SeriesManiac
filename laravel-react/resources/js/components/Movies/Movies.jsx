ef8861af6b67e0610980a46ee243a858b6df24dc
Kapic007 <kapic@bigmir.net>
2021-09-15 17:58:37 +0300

    make some pretty

--- laravel-react/resources/js/components/Movies/Movies.jsx
+++ laravel-react/resources/js/components/Movies/Movies.jsx
@@ -2,49 +2,56 @@ import React, { useEffect, useState } from 'react';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { getTopMovies, loadMoreMovies, searchMovies } from '../../store/actions/movies';
+import {
+  getTopMovies,
+  loadMoreMovies,
+  searchMovies,
+} from '../../store/actions/movies';
 import { MovieListItem } from './MovieListItem/MovieListItem';
 import Header from '../Header/Header';
-import Spinner from 'react-bootstrap/Spinner'
-import './movies.css'
+import Spinner from 'react-bootstrap/Spinner';
 import { RandomMovie } from './RandomMovie/RandomMovie';
 import { toast } from 'react-toastify';
+import './movies.css';
 
 const MINIMUM_QUERY_LENGTH_FOR_SEARCH = 0;
 
 export const Movies = () => {
   const dispatch = useDispatch();
-  const { movies: topMovies} = useSelector((state) => state.moviesReducer);
+  const { movies: topMovies } = useSelector((state) => state.moviesReducer);
   const { preloader, error } = useSelector((state) => state.moviesReducer);
   const [paginateCounter, setPaginateCounter] = useState(0);
   const [searchText, setSearchText] = useState('');
   const [searchQuery, setSearchQuery] = useState(null);
 
   useEffect(() => {
-    if(!topMovies.length){
+    if (!topMovies.length) {
       dispatch(getTopMovies());
     }
   }, []);
 
   useEffect(() => {
     if (error.status) {
-      toast.error(error.errorMessage)
+      toast.error(error.errorMessage);
     }
   }, [error.status]);
 
   const loadMore = () => {
-    dispatch(loadMoreMovies({counter: paginateCounter + 1, query: searchQuery}));
+    dispatch(
+      loadMoreMovies({ counter: paginateCounter + 1, query: searchQuery }),
+    );
     setPaginateCounter(paginateCounter + 1);
-  }
+  };
 
   const handlerSearch = (e) => {
     e.preventDefault();
 
-    if (searchText.trim()?.length > MINIMUM_QUERY_LENGTH_FOR_SEARCH
-        && searchText.trim() !== searchQuery) {
-      dispatch(searchMovies({query: searchText.trim(), counter: 0}));
+    if (
+      searchText.trim()?.length > MINIMUM_QUERY_LENGTH_FOR_SEARCH &&
+      searchText.trim() !== searchQuery
+    ) {
+      dispatch(searchMovies({ query: searchText.trim(), counter: 0 }));
       setSearchQuery(searchText.trim());
-
     } else if (!searchText.trim()?.length) {
       setSearchText('');
       setSearchQuery(null);
@@ -52,7 +59,7 @@ export const Movies = () => {
   };
 
   return (
-    <div className='content'>
+    <div className="content">
       <Header />
 
       {/* Предлагаю пока делать основной функуионал. И.Н. */}
@@ -69,7 +76,10 @@ export const Movies = () => {
         </div>
       </div> */}
       <div className="container-fluid d-flex bg-light justify-content-center">
-        <form className="form-inline col-6 px-2 d-flex justify-content-center" onSubmit={handlerSearch}>
+        <form
+          className="form-inline col-6 px-2 d-flex justify-content-center"
+          onSubmit={handlerSearch}
+        >
           <input
             className="form-control m-2 w-50"
             type="search"
@@ -85,22 +95,30 @@ export const Movies = () => {
       <div className="album py-5 bg-light">
         <div className="container">
           <div id="film-container" className="row align-items-center">
-            {(preloader || !topMovies || !topMovies.length)
-              ? <div className="d-flex justify-content-center align-items-center h-100" ><Spinner animation="border" variant="dark" /></div>
-              : topMovies.map((movie) => {
-                  return (<MovieListItem key={movie.id} {...movie} />)
+            {preloader || !topMovies || !topMovies.length ? (
+              <div className="d-flex justify-content-center align-items-center h-100">
+                <Spinner animation="border" variant="dark" />
+              </div>
+            ) : (
+              topMovies.map((movie) => {
+                return <MovieListItem key={movie.id} {...movie} />;
               })
-            }
+            )}
           </div>
         </div>
         <div className="d-flex justify-content-center align-items-center pt-4">
-          <button onClick={loadMore} id="load-more" type="button" className="btn btn-lg btn-outline-success">
+          <button
+            onClick={loadMore}
+            id="load-more"
+            type="button"
+            className="btn btn-lg btn-outline-success"
+          >
             Load more
           </button>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Movies;
