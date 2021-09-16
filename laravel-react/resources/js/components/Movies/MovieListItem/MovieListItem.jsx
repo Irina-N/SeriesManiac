76d85dd0916f58d5869ecbfed1e2f405f0951f62
Kapic007 <kapic@bigmir.net>
2021-09-16 12:18:59 +0300

    fix some requests & fix link to MoiveCard

--- laravel-react/resources/js/components/Movies/MovieListItem/MovieListItem.jsx
+++ laravel-react/resources/js/components/Movies/MovieListItem/MovieListItem.jsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 import MovieCard from '../MovieCard/MovieCard';
 import './MovieListItem.css'
 
-export const MovieListItem = ({ movieId, image, title, year, ruTitle }) => {
+export const MovieListItem = ({ id, image, title, year, ru_title }) => {
 
   return (
     <Link
       className='col-lg-3 col-md-4 col-12 p-2 movie-list-item'
-      to={`/movie/${movieId}`}>
+      to={`/movies/${id}`}>
 
       <div className="card shadow-sm">
         <img src={image} />
-        <h5 className="card-title">{ruTitle}</h5>
+        <h5 className="card-title">{ru_title}</h5>
         <div className="card-body">
           <p className="card-text truncate">{title}</p>
           <div className="d-flex justify-content-between align-items-center">
