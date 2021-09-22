3014e182a1dd1c9f49a6f400313bf24a636da377
Janis <janisbriedis20@gmail.com>
2021-09-22 14:30:47 +0300

    LoginController edit. Recomment algoritm added to MoviesController

--- laravel-react/app/Http/Controllers/Auth/LoginController.php
+++ laravel-react/app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Grade;
 class LoginController extends Controller
 {
     public function login(Request $request)
@@ -13,14 +14,18 @@ class LoginController extends Controller
         $data = $request->only(['email', 'password']);
 
         if(\Auth::attempt($data)){
-            return response()->json(Auth::user()->only('id','login'),200);
+            $authData = Auth::user()->only('id','login');
+            $movies = Grade::select('movies_id', 'grade')->where('user_id', Auth::id())->get();
+            $authData['userMovies'] = $movies;
+            return response()->json($authData,200);
         }
         
         if(User::where('email', $data['email'])->exists()){
-            return response()->json(['password' => 'Wrong Password!'],400);
+            return response()->json(['message' => 'Wrong Password!'],400);
         }else{
-            return response()->json(['email' => 'Wrong Email!'],400);
+            return response()->json(['message' => 'Wrong Email!'],400);
         }
         
     }
+
 }
