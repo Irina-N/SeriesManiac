776c603613d4197e3827b95196ed550543214221
Janis <janisbriedis20@gmail.com>
2021-09-24 19:56:14 +0300

    User data update add

--- laravel-react/app/Http/Controllers/test.php
+++ laravel-react/app/Http/Controllers/test.php
@@ -8,18 +8,23 @@ use App\Models\Movies;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth; 
 use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Support\Facades\Hash;
 
 class test extends Controller
 {
     public function test()
     {   
-        return $data = [LoginController::class, 'getUserId'];
+        $data = ['userId' => '1','login' => 'greed', 'password' => '12345678'];
+        $user = User::find($data['userId']);
+        $user->password = Hash::make($data['password']);
+        $user->login = $data['login'];
+        $user->save();
     }
 
 
 
     public function test2()
     {
-        return $_POST['userId'];
+        
     }
 }
