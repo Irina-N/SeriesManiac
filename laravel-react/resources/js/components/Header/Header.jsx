d8a7b6adc12e0eec70214ce5ad1055dbeeb709c8
Kapic007 <kapic@bigmir.net>
2021-09-28 17:01:22 +0300

    last fixes

--- laravel-react/resources/js/components/Header/Header.jsx
+++ laravel-react/resources/js/components/Header/Header.jsx
@@ -1,27 +1,22 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 
 import { NavProfile } from './NavProfile/NavProfile';
 
 import './Header.css';
 
 export default function Header() {
-  const history = useHistory();
   const { user } = useSelector((state) => state.currentUserReducer);
 
   return (
-    <header className='header'
-    style={{ justifyContent: user.id ? 'space-between' : 'center' }}>    
-      {/* {user.id && (
-        <i 
-          className='far fa-arrow-alt-circle-left btn-back'
-          onClick={history.goBack}
-        >
-        </i>
-      )} */}
-      <h1 className='header__title'>SериальныйMaньяK</h1>
+    <header
+      className="header"
+      style={{ justifyContent: user.id ? 'space-between' : 'center' }}
+    >
+      <h1 className={`header__title ${user.id ? 'text-left' : 'text-center'}`}>
+        SериальныйMaньяK
+      </h1>
       {user.id && (
         <Navbar>
           <NavProfile userName={user.login} />
